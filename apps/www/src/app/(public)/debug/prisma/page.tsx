331de57c84a01f1c95235ed218f2c918fcefd228
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 16:58:53 +0100

    .

--- apps/www/src/app/(public)/debug/prisma/page.tsx
+++ apps/www/src/app/(public)/debug/prisma/page.tsx
@@ -1,6 +1,12 @@
 import { db } from "@gnd/db";
-
+import { trpc } from "@/trpc/server";
 export default async function Page() {
-    const x = await db.dealerAuth.count({});
-    return <div>{x}</div>;
+  const x = await db.dealerAuth.count({});
+  const { data } = await trpc.user.auth.queryOption();
+  return (
+    <div>
+      {x}
+      <div>{data}</div>
+    </div>
+  );
 }
