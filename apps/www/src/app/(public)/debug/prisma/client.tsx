1d25d235ecc5815198eaf790ce6a49c8407baa1e
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 19:21:33 +0100

    .

---
+++ apps/www/src/app/(public)/debug/prisma/client.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { _trpc } from "@/components/static-trpc";
+import { Button } from "@gnd/ui/button";
+import { useQuery } from "@gnd/ui/tanstack";
+import { useState } from "react";
+
+export function Client() {
+  const [enabled, setEnabled] = useState(false);
+  const { data, isPending } = useQuery(
+    _trpc.user.auth.queryOptions(undefined),
+    {
+      enabled,
+    }
+  );
+  //   const { data } = await qc.prefetchQuery(trpc.user.auth.queryOptions());
+  const r = JSON.stringify({ data, isPending });
+  return (
+    <div>
+      {/* {x} */}
+      <div>{r}</div>
+      <Button
+        onClick={(e) => {
+          setEnabled(!enabled);
+        }}
+      >
+        Fetch
+      </Button>
+    </div>
+  );
+}
--- apps/www/src/app/(public)/debug/prisma/page.tsx
+++ apps/www/src/app/(public)/debug/prisma/page.tsx
@@ -1,16 +1,19 @@
-"use client";
+// import { _trpc } from "@/components/static-trpc";
+// import { useQuery } from "@gnd/ui/tanstack";
 
-import { _trpc } from "@/components/static-trpc";
-import { useQuery } from "@gnd/ui/tanstack";
+import { db } from "@gnd/db";
+import { Client } from "./client";
 
 export default async function Page() {
-  const { data, isPending } = useQuery(_trpc.user.auth.queryOptions());
+  //   const { data, isPending } = useQuery(_trpc.user.auth.queryOptions());
   //   const { data } = await qc.prefetchQuery(trpc.user.auth.queryOptions());
-  const r = JSON.stringify({ data, isPending });
+  //   const r = JSON.stringify({ data, isPending });
+  const r = await db.users.count({});
   return (
     <div>
       {/* {x} */}
       <div>{r}</div>
+      <Client />
     </div>
   );
 }
