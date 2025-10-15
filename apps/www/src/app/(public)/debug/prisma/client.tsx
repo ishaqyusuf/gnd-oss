ca1420a142a8b8fd4e6d583391f17a88d3c32d73
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 20:52:43 +0100

    ...

--- apps/www/src/app/(public)/debug/prisma/client.tsx
+++ apps/www/src/app/(public)/debug/prisma/client.tsx
@@ -3,6 +3,7 @@
 import { _trpc } from "@/components/static-trpc";
 import { Button } from "@gnd/ui/button";
 import { useQuery } from "@gnd/ui/tanstack";
+import { signIn } from "next-auth/react";
 import { useState } from "react";
 
 export function Client() {
@@ -16,10 +17,20 @@ export function Client() {
       <div>{r}</div>
       <Button
         onClick={(e) => {
-          setEnabled(!enabled);
+          // setEnabled(!enabled);
+          try {
+            signIn("credentials", {
+              callbackUrl: "/debug/prisma",
+              redirect: false,
+              email: "ishaqyusuf",
+              password: "....",
+            });
+          } catch (e) {
+            console.log(e);
+          }
         }}
       >
-        Fetch
+        Sign in
       </Button>
     </div>
   );
