5f592b8b60deabe4f950c2756a999510f380fd43
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 20:11:31 +0100

    .

--- apps/www/src/db/index.ts
+++ apps/www/src/db/index.ts
@@ -2,7 +2,5 @@ import { db } from "@gnd/db";
 
 export { type Prisma } from "@gnd/db";
 
-// export const Prisma = BasePrisma;
-
 export * from "@gnd/db";
-export { db as prisma }; //;///= db;
+export { db as prisma };
