5e9bd383af849589cc159c5fbeb03ff90e49ec6f
Ishaq Yusuf <ishaqyusuf024@gmail.com>
2025-10-15 17:17:28 +0100

    .

--- apps/www/src/app/(public)/debug/prisma/page.tsx
+++ apps/www/src/app/(public)/debug/prisma/page.tsx
@@ -4,12 +4,13 @@ import { _trpc } from "@/components/static-trpc";
 import { useQuery } from "@gnd/ui/tanstack";
 
 export default async function Page() {
-  const { data } = useQuery(_trpc.user.auth.queryOptions());
+  const { data, isPending } = useQuery(_trpc.user.auth.queryOptions());
   //   const { data } = await qc.prefetchQuery(trpc.user.auth.queryOptions());
+  const r = JSON.stringify({ data, isPending });
   return (
     <div>
       {/* {x} */}
-      <div>{data}</div>
+      <div>{r}</div>
     </div>
   );
 }
